adef88ee6af68f94e6492b0cf48f09a13fa4b5bd
Amanda Briones <amandabriones111@gmail.com>
2022-06-06 00:44:52 -0700

    commit tree e9eb689eef94e64aa6e9599691121484cd1df7e3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Amanda Briones';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
